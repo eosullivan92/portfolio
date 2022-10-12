32cad40bd3d8e3926c5fbdc183fb2b9690b2ac8e
Eamonn O'Sullivan <eamonnsosullivan@gmail.com>
2022-10-12 14:33:41 +1300

    update bio

--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -11,13 +11,14 @@ export default function Hero() {
           <h3 className="subtitle">Web Developer</h3>
 
           <div className="about">
-            I am a dynamic and challenge-driven professional transitioning from
-            the travel and accommodation sector into a career in Software
-            Development with a focus on Front End Web Development. I am
-            passionate about continuous improvement, not only for my teams but
-            for myself, and I am committed to learning new skills and
-            capabilities to support my colleagues in overcoming complex
-            problems.
+            I am a dynamic and challenge-driven professional with a love for
+            Front End Web Development. I started my Web Development journey in
+            September 2021 and found myself instantly captivated by the endless
+            opportunities for creativity and problem solving when creating fun
+            and satisfying user experiences. I am passionate about continuous
+            improvement, not only for my teams but for myself, and I am
+            committed to learning new skills and capabilities to support my
+            colleagues in overcoming complex problems.
           </div>
           <SocialLinks />
         </div>
